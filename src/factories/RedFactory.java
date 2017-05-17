8788d52144f192f41773061ee3eec44280910d30
andrewyashin <razzer9876@gmail.com>
2017-05-17 12:09:04 +0300

    fix RedFactory

--- src/factories/RedFactory.java
+++ src/factories/RedFactory.java
@@ -12,12 +12,6 @@ public class RedFactory implements BaseFactory {
         return new RedCircle(radius);
     }
 
-    @Override
-    public Triangle createTriangle() {
-        return null;
-    }
-
-
 
     public Triangle createTriangle(double a, double b, double c) {
         return new RedTriangle(a, b, c);
