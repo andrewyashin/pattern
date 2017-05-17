544a97fe5ca52941fd37a6163ae76b23af2848fe
Tetianka <t.fomitskaya@gmail.com>
2017-05-17 13:04:16 +0300

    Drawing coloured circles feature added

--- src/impl/BlackCircle.java
+++ src/impl/BlackCircle.java
@@ -9,12 +9,6 @@ public class BlackCircle implements Circle {
         this.radius = radius;
     }
 
-    //TODO: #1 Draw shape with * or only print type of shape and it Color
-    @Override
-    public String draw() {
-        return null;
-    }
-
     @Override
     public double getPerimetr() {
         return Math.PI * 2 * radius;
@@ -23,5 +17,25 @@ public class BlackCircle implements Circle {
     @Override
     public double getSquare() {
         return Math.PI * radius * radius;
+    }
+    //TODO: #1 Draw shape with * or only print type of shape and it Color
+    @Override
+    public String draw() {
+        int radiusIntValue = (int) this.radius;
+        int coordinateX = 5;
+        int coordinateY = 15;
+        for (int i = 0;i <= coordinateX + radiusIntValue; i++) {
+            for (int j = 1;j <=coordinateY + radiusIntValue; j++) {
+                int xSquared = (i - coordinateX)*(i - coordinateX);
+                int ySquared = (j - coordinateY)*(j - coordinateY);
+                if (Math.abs(xSquared + ySquared - radiusIntValue * radiusIntValue) < radiusIntValue) {
+                    System.out.print((char)27 + "[30m*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        return "Black Circle";
     }
 }
--- src/impl/RedCircle.java
+++ src/impl/RedCircle.java
@@ -6,7 +6,6 @@ public class RedCircle implements Circle {
 
     public RedCircle(double radius) {
         this.radius = radius;
-
     }
 
     @Override
@@ -18,6 +17,26 @@ public class RedCircle implements Circle {
     public double getSquare() {
         return Math.PI * Math.pow(radius, 2);
     }
-}
+
+    @Override
+    public String draw() {
+        int radiusIntValue = (int) this.radius;
+        int coordinateX = 5;
+        int coordinateY = 15;
+        for (int i = 0;i <= coordinateX + radiusIntValue; i++) {
+            for (int j = 1;j <=coordinateY + radiusIntValue; j++) {
+                int xSquared = (i - coordinateX)*(i - coordinateX);
+                int ySquared = (j - coordinateY)*(j - coordinateY);
+                if (Math.abs(xSquared + ySquared - radiusIntValue * radiusIntValue) < radiusIntValue) {
+                    System.out.print((char)27 + "[31m*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        return "Red Circle";
+    }
+    }
 
 
